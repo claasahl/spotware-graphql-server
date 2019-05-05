ab2a2177a6125a5b320659acaeb3fb48a27c53b0
Claas Ahlrichs <claasahl@gmail.com>
2019-05-05 12:51:27 +0200

    use generated typings

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,16 +1,20 @@
 import { GraphQLServer } from "graphql-yoga";
 import fs from "fs";
+
+import { Resolvers } from "./generated/graphql";
+import { IResolvers } from "graphql-tools";
+
 const typeDefs = () => {
   const buffer = fs.readFileSync("./src/schema/hello.graphql");
   return [buffer.toString()];
 };
 
-const resolvers = {
+const resolvers: Resolvers & IResolvers = {
   Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || "World"}`
+    hello: (_, { name }) => `Hello ${name || "World"}`
   }
 };
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ typeDefs, resolvers: resolvers });
 server.start(options =>
   console.log(`server is listening on http://localhost:${options.port}`)
 );
